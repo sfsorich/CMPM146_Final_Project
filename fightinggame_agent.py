32a425e6c1d807e62a6390eb5b7750752fc21eda
Stephen Sorich <ssorich@ucsc.edu>
2018-12-13 15:31:29 -0800

    added reward value to neural network

--- fightinggame_agent.py
+++ fightinggame_agent.py
@@ -3,71 +3,75 @@
 import argparse
 import retro
 import numpy
+from collections import deque
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
 
 #hyperparamters for Q learning
 gamma = 0.95
-learning_rate = 0.001
+learning_rate = 0.1
 max_memory = 10000
 # Exploration parameters for epsilon greedy strategy
-explore_start = 1.0            # exploration probability at start
-explore_stop = 0.01            # minimum exploration probability
+epsilon = 1.0            # exploration probability at start
+epsilon_min = 0.01            # minimum exploration probability
 decay_rate = 0.0001            # exponential decay rate for exploration prob
 #number of episodes to run
 episodes = 250
 
 
-# def shared():
-#     t += 1
-#     if t % 10 ==0:
-#         print('INFO: {}'.format(info))
-#         timer = info['timer']
-#
-#     if args.players == 1:
-#         rew = [rew]
-#
-#     for i, r in enumerate(rew):
-#         totrew[i] += r
-#         if verbosity > 0:
-#             if r > 0:
-#                 print('t=%i p=%i got reward: %g, current reward: %g' % (t, i, r, totrew[i]))
-#             if r < 0:
-#                 print('t=%i p=%i got penalty: %g, current reward: %g' % (t, i, r, totrew[i]))
-#     if done:
-#         env.render()
-#         try:
-#             if verbosity >= 0:
-#                 if args.players > 1:
-#                     print("done! total reward: time=%i, reward=%r" % (t, totrew))
-#                 else:
-#                     print("done! total reward: time=%i, reward=%d" % (t, totrew[0]))
-#                 input("press enter to continue")
-#                 print()
-#             else:
-#                 input("")
-#         except EOFError:
-#             exit(0)
+# stack_size = 4
+# state_stack = deque()
+# def stack_states(state, state_stack):
+
+network = Sequential()
+network.add(Dense(36, input_shape= (6,), activation='relu')) #basic input dimensions to neural net and layers
+network.add(Dense(24, activation='relu'))
+network.add(Dense(12, activation='sigmoid')) #output layer
+network.compile(loss='mse', optimizer=Adam(lr=learning_rate)) #need to setup the learning functions for this to create network
+
+memory = deque([], maxlen=max_memory)
+def remember(state, action, reward, next_state, done):
+    memory.append((state, action, reward, next_state, done))
+
+def act(state):
+    global epsilon
+    global network
+    if numpy.random.rand() <= epsilon:
+        return env.action_space.sample()
+    epsilon -= decay_rate
+    act_values = network.predict(state.reshape((1, -1)))
+    act_values[act_values < 0.5] = 0
+    act_values[act_values >= 0.5] = 1
+    return act_values
+
+def replay(batch_size):
+    if len(memory) < batch_size:
+        return
+    minibatch = random.sample(memory, batch_size)
+    for state, action, rew, next_state, done in minbatch:
+        target = network.predict(state.reshape(1, -1))
+        if done:
+            target[0] = reward
+        else:
+            Q_next = max(network.predict(next_state.reshape(1, -1)))
+
+
 
 
 def main():
     #given by random agent.py setting up gym retro usage
     parser = argparse.ArgumentParser()
     parser.add_argument('game', default='SuperStreetFighterIITurboRevival-GbAdvance', help='the name or path for the game to run')
-    parser.add_argument('state', nargs='?', help='the initial state file to load, minus the extension')
+    parser.add_argument('state', nargs='?', default=retro.State.DEFAULT, help='the initial state file to load, minus the extension')
     parser.add_argument('--players', '-p', type=int, default=1, help='number of players/agents (default: 1)')
     args = parser.parse_args()
 
-    env = retro.make(args.game, args.state or retro.State.DEFAULT, players=args.players)
-
-
-
-    network = Sequential()
-    network.add(Dense(36, input_shape= (6,), activation='relu')) #basic input dimensions to neural net and layers
-    network.add(Dense(12, activation='sigmoid')) #output layer
-    network.compile(loss='mse', optimizer=Adam(lr=learning_rate)) #need to setup the learning functions for this to create network
+    global env
+    env = retro.make(args.game, args.state, players=args.players)
     #print(env.observation_space)
+
+
     training = True
 
     try:
@@ -77,38 +81,37 @@ def main():
             ob, rew, done, info = env.step(action) #take the action we think will result in a reward
             t = 0
             totrew = [0] * args.players
-            if ep < 5: #for the first 20 episodes take random actions to pretrain the network
+
+            state_old = numpy.array([info['x-p1'], info['y-p1'], info['x-p2'], info['y-p2'], info['state-p1'], info['state-p2']]) #this is our observation_space
+            if ep < 0: #for the first 20 episodes take random actions to pretrain the network
                 while True:
                     action = env.action_space.sample() #this is where DQ function evaluates which action to take
                     action_hold = numpy.zeros(12)
-                    #print(action_hold)
-                    #print(info)
-                    state = numpy.array([info['x-p1'], info['y-p1'], info['x-p2'], info['y-p2'], info['state-p1'], info['state-p2']]) #this is our observation_space
-                    #print(state.shape)
-                    #print(state.reshape((1,-1)))
-                    network.fit(state.reshape((1,-1)), action_hold.reshape((1,-1)), epochs=1)
                     ob, rew, done, info = env.step(action) #take the action we think will result in a reward
-                    #env.render()
+                    state_new = numpy.array([info['x-p1'], info['y-p1'], info['x-p2'], info['y-p2'], info['state-p1'], info['state-p2']]) #this is our observation_space
+                    print(numpy.multiply(rew, action).reshape((1,-1)))
+                    remember(state_old, action, rew, state_new, done)
+                    network.fit(state_old.reshape((1,-1)), numpy.multiply(rew, action).reshape((1,-1)), epochs=1)
+                    state_old = state_new
+                    env.render()
                     if done:
+                        print("============== EPISODE {} FINISHED ==============".format(ep))
                         break
 
             else :
                 while True:
-
-                    state =numpy.array( [info['x-p1'], info['y-p1'], info['x-p2'], info['y-p2'], info['state-p1'], info['state-p2']])
+                    action = act(state_old)
                     action_hold = numpy.zeros(12)
-
-                    #network.predict(state)
-                    action = network.predict(state.reshape(1, -1))
+                    ob, rew, done, info = env.step(action) #take the action we think will result in a reward
+                    if (info['health-p2'] < info['health-p1']): rew += 5.0
+                    if (info['health-p2'] > info['health-p1']): rew -= 7.5
                     print(action)
-                    action[action < 0.5] = 0
-                    action[action >= 0.5] = 1
-                    print(action)
-                    ob, rew, done, info = env.step(action[0]) #take the action we think will result in a reward
-                    network.fit(state.reshape((1,-1)), action_hold.reshape((1,-1)), epochs=1)
-                    print(action_hold)
+                    print("REWARD: {}".format(rew))
+                    network.fit(state_old.reshape((1,-1)), numpy.multiply(rew, action).reshape((1,-1)), epochs=1)
+                    #print(action_hold)
                     env.render()
                     if done:
+                        print("============== EPISODE {} FINISHED ==============".format(ep))
                         break
 
     except KeyboardInterrupt:
@@ -116,3 +119,36 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+    # def shared():
+    #     t += 1
+    #     if t % 10 ==0:
+    #         print('INFO: {}'.format(info))
+    #         timer = info['timer']
+    #
+    #     if args.players == 1:
+    #         rew = [rew]
+    #
+    #     for i, r in enumerate(rew):
+    #         totrew[i] += r
+    #         if verbosity > 0:
+    #             if r > 0:
+    #                 print('t=%i p=%i got reward: %g, current reward: %g' % (t, i, r, totrew[i]))
+    #             if r < 0:
+    #                 print('t=%i p=%i got penalty: %g, current reward: %g' % (t, i, r, totrew[i]))
+    #     if done:
+    #         env.render()
+    #         try:
+    #             if verbosity >= 0:
+    #                 if args.players > 1:
+    #                     print("done! total reward: time=%i, reward=%r" % (t, totrew))
+    #                 else:
+    #                     print("done! total reward: time=%i, reward=%d" % (t, totrew[0]))
+    #                 input("press enter to continue")
+    #                 print()
+    #             else:
+    #                 input("")
+    #         except EOFError:
+    #             exit(0)
